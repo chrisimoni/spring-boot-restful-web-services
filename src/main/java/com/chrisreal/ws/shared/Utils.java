01fdd3df5b693b6c5e9268d975aafd18b4d9b85d
Imoni Christopher <imonireal@gmail.com>
2019-10-06 00:46:56 +0100

    generate public address Id

--- src/main/java/com/chrisreal/ws/shared/Utils.java
+++ src/main/java/com/chrisreal/ws/shared/Utils.java
@@ -14,6 +14,10 @@ public class Utils {
 	public String generateUserId(int length) {
 		return generatedRandomString(length);
 	}
+	
+	public String generateAddressId(int length) {
+		return generatedRandomString(length);
+	}
 
 	private String generatedRandomString(int length) {
 		StringBuilder returnValue = new StringBuilder(length);
