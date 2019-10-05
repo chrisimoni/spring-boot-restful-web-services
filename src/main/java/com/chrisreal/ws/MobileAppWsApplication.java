ec9458acea613a2c6c497a5f19809a564b1d4877
Imoni Christopher <imonireal@gmail.com>
2019-10-05 13:22:50 +0100

    add support for generating WAR file

--- src/main/java/com/chrisreal/ws/MobileAppWsApplication.java
+++ src/main/java/com/chrisreal/ws/MobileAppWsApplication.java
@@ -2,16 +2,25 @@ package com.chrisreal.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.chrisreal.ws.security.AppProperties;
 
 @SpringBootApplication
-public class MobileAppWsApplication {
-
+public class MobileAppWsApplication extends SpringBootServletInitializer {
+	
+	//To generate WAR, add extends SpringBootServletInitializer and override SpringApplicationBuilder configure
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MobileAppWsApplication.class);
 	}
 	
 	@Bean
