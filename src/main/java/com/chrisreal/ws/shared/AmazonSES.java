0fe9c341205430fd030ffbdcd6813eb659ebc8e6
Imoni Christopher <imonireal@gmail.com>
2019-10-30 03:13:55 +0100

    update AmazonSES with code that sends password reset email

--- src/main/java/com/chrisreal/ws/shared/AmazonSES.java
+++ src/main/java/com/chrisreal/ws/shared/AmazonSES.java
@@ -10,6 +10,7 @@ import com.amazonaws.services.simpleemail.model.Content;
 import com.amazonaws.services.simpleemail.model.Destination;
 import com.amazonaws.services.simpleemail.model.Message;
 import com.amazonaws.services.simpleemail.model.SendEmailRequest;
+import com.amazonaws.services.simpleemail.model.SendEmailResult;
 import com.chrisreal.ws.shared.dto.UserDto;
 
 public class AmazonSES {
@@ -18,6 +19,8 @@ public class AmazonSES {
 	
 	//The subject for the email;
 	final String SUBJECT = "One last step to complete your registration";
+	
+	final String PASSWORD_RESET_SUBJECT = "Password reset request";
 	
 	//HTML body for the email
 	final String HTMLBODY = "<h1>Please verify your email address</h1>"
@@ -32,6 +35,21 @@ public class AmazonSES {
 			+ "Thank you for registering with our app. To complete your registraton process and be able to log in, "
 			+ " Open the following URL in your browser:"
 			+ "http://ec2-34-203-247-65.compute-1.amazonaws.com:8080/verification-service/email-verification.html?token=$tokenValue"
+			+ "Thank you!";
+	
+	final String PASSWORD_RESET_HTMLBODY = "<h1>A request to reset your password</h1>"
+			+ "<p>Hi, $firstName!</p> "
+			+ "<p>Someone has requested to reset your password. If you are not the one please ignore. "
+			+ " Otherwise please click on the following link to set a new password:"
+			+ "<a href='http://localhost.com:8080/verification-service/password-reset.html?token=$tokenValue'>"
+			+ "Click this link to reset your password</a><br/><br/>"
+			+ "Thank you!</p>";
+	
+	final String PASSWORD_RESET_TEXTBODY = "A request to reset your password "
+			+ "Hi, $firstName! "
+			+ "Someone has requested to reset your password. If you are not the one please ignore. "
+			+ " Otherwise please open the following link to set a new password: "
+			+ "http://localhost.com:8080/verification-service/password-reset.html?token=$tokenValue "
 			+ "Thank you!";
 	
 	public void verifyEmail(UserDto userDto) {
@@ -53,4 +71,45 @@ public class AmazonSES {
 		
 		System.out.println("Email sent");
 	}
+
+	
+	public boolean sendPasswordResetRequest(String firstName, String email, String token) {
+		boolean returnValue = false;
+		
+		BasicAWSCredentials awsCreds = new BasicAWSCredentials("enter-your-id", "enter-your-secret");
+		AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder
+				.standard()
+				.withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+				.withRegion(Regions.US_EAST_1)
+				.build();
+		
+		String htmlBodyWithToken = PASSWORD_RESET_HTMLBODY.replace("$tokenValue", token);
+		htmlBodyWithToken = htmlBodyWithToken.replace("$firstName", firstName);
+		
+		String textBodyWithToken = PASSWORD_RESET_TEXTBODY.replace("$tokenValue", token);
+		textBodyWithToken = textBodyWithToken.replace("$firstName", firstName);
+		
+		SendEmailRequest request = new SendEmailRequest()
+				.withDestination(
+						new Destination().withToAddresses(email))
+				.withMessage(new Message()
+						.withBody(new Body()
+								.withHtml(new Content()
+										.withCharset("UTF-8").withData(htmlBodyWithToken))
+								.withText(new Content()
+										.withCharset("UTF-8").withData(textBodyWithToken)))
+						.withSubject(new Content()
+								.withCharset("UTF-8").withData(PASSWORD_RESET_SUBJECT)))
+				.withSource(FROM);
+		
+		
+		SendEmailResult result = client.sendEmail(request);
+		
+		if(result !=null && (result.getMessageId() != null && !result.getMessageId().isEmpty())) {
+			returnValue = true;
+		}
+		
+		return returnValue;
+	}
+
 }
